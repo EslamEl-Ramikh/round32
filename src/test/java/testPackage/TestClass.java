e7a52618fdb0d165100574b4762039bdb214db79
EslamEl-Ramikh <97061454+EslamEl-Ramikh@users.noreply.github.com>
2025-08-23 14:45:34 +0300

    Task 1 & 2 Commit: Task1: "Test to fail" asserting current title and then close the browser
    Task2: Verifying that the logo image is displayed

--- src/test/java/testPackage/TestClass.java
+++ src/test/java/testPackage/TestClass.java
@@ -1,19 +1,43 @@
-
-
-
 package testPackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class TestClass {
 
+
+    RemoteWebDriver driver = new ChromeDriver();
     @Test
     public void test()
     {
-        WebDriver driver = new ChromeDriver();
+
+
+            driver.get("https://duckduckgo.com/");
+            String ActualTitle = driver.getTitle();
+            String ExpectedTitle = ("Google");
+            Assert.assertEquals(ActualTitle, ExpectedTitle, "Page title doesn't match");
+
     }
 
+    @Test
+    public void VerifyingLogoisDisplayed()
+    {
+        driver.get("https://duckduckgo.com/");
+        WebElement logo = driver.findElement(By.xpath("//section[@class='header_headerLeft__rW6nD header_headerSection___XMRI']//img"));
+        Assert.assertTrue(logo.isDisplayed(),"The logo is not displayed");
+    }
+
+
+    @AfterMethod
+    public void tearDown()
+    {
+        driver.quit();
+    }
 
 }
